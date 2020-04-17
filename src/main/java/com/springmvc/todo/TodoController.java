28a0052f890e8bf856c7da070bdef6588261dc58
panjingya <panjingya0929@gmail.com>
2020-04-17 11:31:36 -0400

    add a high level exception handling using ControllerAdvice

--- src/main/java/com/springmvc/todo/TodoController.java
+++ src/main/java/com/springmvc/todo/TodoController.java
@@ -2,7 +2,10 @@ package com.springmvc.todo;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,16 +14,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import com.springmvc.exception.ExceptionController;
 
 @Controller
 @SessionAttributes("name")
 public class TodoController {
 
+  private Log logger = LogFactory.getLog(TodoController.class);
   @Autowired
   TodoService service;
   
@@ -44,6 +50,8 @@ public class TodoController {
   }
   @RequestMapping(value = "/add-todo", method = RequestMethod.GET)
   public String showTodoPage(ModelMap model) {
+    //Trial--trigger exception
+    //throw new RuntimeException("dummy");
     model.put("todo", new Todo(0, retrieveLoggedinUserName(), "Default Desc", new Date(), false));
     return "todo";
   }
@@ -75,5 +83,10 @@ public class TodoController {
     }
     service.updateTodo(todo);
     return "redirect:list-todos";
+  }
+  @ExceptionHandler(value=Exception.class)
+  public String exceptionHanlder(HttpServletRequest request, Exception ex) {
+    logger.error("Request"+request.getRequestURL()+"Thorw an Exception", ex);
+    return "error-specific";
   }
 }
